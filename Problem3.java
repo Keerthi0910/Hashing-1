08d5ffb6a7260f3fca78df594c347b154fa7e243
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-05 23:55:08 -0500

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,31 @@
 
+o(n) time complexity
+O(2n) space complexity for hashmap and hashset
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        if(words.length != pattern.length()){
+            return false;
+        }
+        for(int i = 0; i<pattern.length(); i++){
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(words[i])){
+                     return false;
+                }
+            } else {
+                if(set.contains(words[i])){
+                    return false;
+                }
+            }
+
+            set.add(words[i]);
+            map.put(ch, words[i]);
+        }
+        return true;
+
+    }
+}
