c618e6ce8141c7a1c644e352da3ebfc46630aa66
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-05 23:58:22 -0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,26 @@
 
+// Group anagrams
+//O(n) spave complexity for extra hash map space
+// O(n) time complexity
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] strCharArray = str.toCharArray();
+            Arrays.sort(strCharArray);
+            String strSortedString = new String(strCharArray);
+            if(!map.containsKey(strSortedString)){
+                map.put(strSortedString, new ArrayList<>());
+            }
+      
+            
+                map.get(strSortedString).add(str);
+            
+
+        }
+        return new ArrayList<>(map.values());
+    }
+}
